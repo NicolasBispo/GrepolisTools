df98dd9a2a1f147ff242cf81e483df308e47b4b9
Nícolas <48634498+NicolasBispo@users.noreply.github.com>
2023-05-30 18:53:58 -0300

    Implementação da requisição enviarLogin e iniciarPuppeteer

--- api-server/server.js
+++ api-server/server.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const GrepolisPuppeteer = require('./../puppeteer/GrepolisPuppeteer');
 
+const grepolis = new GrepolisPuppeteer();
 // Configuração do middleware para processar JSON
 app.use(express.json());
 
+app.post('/iniciarNavegador', (req, res) => {
+
+    async function executar() {
+
+        await grepolis.iniciarPuppeteer();
+        res.json({
+            startserver: true
+        });
+    }
+    executar();
+
+});
 // Defina as rotas da sua API
 app.post('/enviarLogin', (req, res) => {
-    const { login, password } = req.body;
+    const login = req.body.login;
+    const password = req.body.password;
+
+    async function executar() {
+        const loggedStatus = await grepolis.executarLogin(login, password);
+        console.log('Valor de logged status: ' + loggedStatus)
+        console.log('Método executarLogin concluído.');
+        if (loggedStatus) {
+            res.json({
+                message: true
+            })
+        }
+        else {
+            res.json({
+                message: false
+            })
+        }
+
+    }
+    executar();
+
 
 
 });
