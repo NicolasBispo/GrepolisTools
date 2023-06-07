0a679cd3deed1fde7e40702d46386541e6c2dfe9
Nícolas <48634498+NicolasBispo@users.noreply.github.com>
2023-06-07 11:18:25 -0300

    Implementação de mecanismos de início do navegador e login

--- electron/assets/JS/index_control.js
+++ electron/assets/JS/index_control.js
@@ -3,9 +3,6 @@ const botaoLogin = document.querySelector('#loginButton');
 botaoLogin.addEventListener('click', () => {
     // Exibir a tela de carregamento
 
-
-
-
     const login = document.querySelector('#login_user').value;
     const password = document.querySelector('#password_user').value;
 
@@ -30,13 +27,28 @@ botaoLogin.addEventListener('click', () => {
 
             if (result.message === true) {
                 // Usuário logado com sucesso
-                window.location.href = "./../../templates/loginsucess.html";
-                console.log('Logado');
+                loader.style.display = 'none';
+                const worldsContainer = document.querySelector('.worlds-container');
+                const worldsContainerDiv = document.querySelector('.worlds-container div');
+
+                result.mundos.forEach(mundo => {
+                    const buttonWorld = document.createElement('button');
+                    buttonWorld.textContent = mundo;
+                    buttonWorld.addEventListener('click', () => {
+                        worldsContainer.style.display = 'none';
+                        loader.style.display = 'block';
+                        requisicaoMundo(mundo);
+                    })
+                    worldsContainerDiv.appendChild(buttonWorld);
+                });
+                worldsContainer.style.display = 'block';
+
             } else {
                 // Falha no login
                 loader.style.display = 'none';
                 loginArea.style.display = 'block';
-                console.log('Não logado');
+                const smallLogin = document.querySelector('#smallLogin');
+                smallLogin.style.display = 'block';
             }
         })
         .catch(error => {
@@ -49,6 +61,11 @@ botaoLogin.addEventListener('click', () => {
 });
 
 function requisicaoInicial() {
+    const loader = document.querySelector('.loader');
+    const loginArea = document.querySelector('.grepolis-contents');
+
+    loginArea.style.display = 'none';
+    loader.style.display = 'block';
     fetch('http://localhost:3000/iniciarNavegador', {
         method: 'POST',
         headers: {
@@ -62,14 +79,50 @@ function requisicaoInicial() {
 
             if (result.startserver === true) {
                 //Servidor inicializado com sucesso
-                console.log('servidor inicializado')
+                loginArea.style.display = 'flex';
+                loader.style.display = 'none';
             }
         })
         .catch(error => {
             console.error('Erro na inicialização do server:', error);
         });
 }
+function requisicaoMundo(mundo) {
+    fetch('http://localhost:3000/requisicaoMundo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ mundo: mundo })
+    })
+        .then(response => response.json())
+        .then(result => {
 
+
+            if (result.complete === true) {
+                fetch('http://localhost:3000/atualizarLogin', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: ''
+                })
+                    .then(response => response.json())
+                    .then(result => {
+                        
+                    })
+                    .catch(error => {
+                        console.error('Erro:', error);
+                    });
+
+            } else {
+                console.log('Erro kkkk')
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
 window.onload = function () {
     requisicaoInicial();
 }
